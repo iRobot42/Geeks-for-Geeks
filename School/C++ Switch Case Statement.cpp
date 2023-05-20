08c0480eddb092f49b7eb7438f7ccf2aa32735a5
Alexander Kobez <alexander.kobez@gmail.com>
2023-05-20 16:20:06 +0300

    Create C++ Switch Case Statement.cpp

---
+++ School/C++ Switch Case Statement.cpp
@@ -0,0 +1,15 @@
+string isInRange( int n ) {
+    switch ( n ) {
+        case 1 : return "one";
+        case 2 : return "two";
+        case 3 : return "three";
+        case 4 : return "four";
+        case 5 : return "five";
+        case 6 : return "six";
+        case 7 : return "seven";
+        case 8 : return "eight";
+        case 9 : return "nine";
+        case 10 : return "ten";
+        default : return "not in range";
+    }
+} // T: 0.01
