d47c52ad19163f6ecac4a9adfb587d395e7681c7
Alexander Kobez <alexander.kobez@gmail.com>
2023-05-21 02:50:21 +0300

    Create Floyd's triangle.cpp

---
+++ School/Floyd's triangle.cpp
@@ -0,0 +1,7 @@
+void printFloydTriangle( int n ) {
+    for ( int i{}, c{ 1 }; i < n; ++i ) {
+        for ( int j{}; j <= i; ++j )
+            cout << c++ << ' ';
+        cout << endl;
+    }
+} // T: 0.02 - 0.03
