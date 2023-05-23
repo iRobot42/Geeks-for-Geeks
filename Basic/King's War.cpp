dd200e583b53ede4c1454538171819e5e09c0de8
Alexander Kobez <alexander.kobez@gmail.com>
2023-05-23 04:53:56 +0300

    Update King's War.cpp

--- Basic/King's War.cpp
+++ Basic/King's War.cpp
@@ -1,6 +1,5 @@
 long long SoldierRequired( long long a[], long long n ) {
-    const long long w{ *min_element( a, a + n ) },
-                    s{ *max_element( a, a + n ) };
+    const auto m{ minmax_element( a, a + n ) };
     return count_if( a, a + n,
-        [ & ]( int i ){ return i != w && i != s; } );
+        [ & ]( int i ){ return i != *m.first && i != *m.second; } );
 } // T: 0.48 - 0.56
