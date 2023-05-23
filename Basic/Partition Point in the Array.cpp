9bc13b57fe9b1ce4a6af950213d6553ca9183ada
Alexander Kobez <alexander.kobez@gmail.com>
2023-05-24 01:35:50 +0300

    Create Partition Point in the Array.cpp

---
+++ Basic/Partition Point in the Array.cpp
@@ -0,0 +1,11 @@
+int FindElement( int a[], int n ) {
+    vector< int > g( n ), s( n, 1e6 );
+    for ( int i{ 1 }, j{ n - 2 }; i < n; ++i, --j ) {
+        g[ i ] = max( g[ i - 1 ], a[ i - 1 ] );
+        s[ j ] = min( s[ j + 1 ], a[ j + 1 ] );
+    }
+    for ( int i{}; i < n; ++i )
+        if ( a[ i ] > g[ i ] && a[ i ] < s[ i ] )
+            return a[ i ];
+    return -1;
+} // T: 0.10 - 0.12
