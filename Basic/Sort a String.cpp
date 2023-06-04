0075aad85548dd790bf044caf82467807dc84425
Alexander Kobez <alexander.kobez@gmail.com>
2023-06-04 07:23:59 +0300

    Create Sort a String.cpp

---
+++ Basic/Sort a String.cpp
@@ -0,0 +1,5 @@
+// "The standard library saves programmers from having to reinvent the wheel."
+// Bjarne Stroustrup
+string sort( string s ) {
+    return std::sort( s.begin(), s.end() ), s;
+} // T: 0.39+
