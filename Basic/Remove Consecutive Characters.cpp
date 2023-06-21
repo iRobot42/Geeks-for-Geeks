22edf15885ddc7179ce2b4a3c647f02473dc77f8
Alexander Kobez <alexander.kobez@gmail.com>
2023-06-21 04:02:32 +0300

    Create Remove Consecutive Characters.cpp

---
+++ Basic/Remove Consecutive Characters.cpp
@@ -0,0 +1,20 @@
+#define V2
+
+#ifdef V1 // SC: O(n)
+string removeConsecutiveCharacter( string s ) {
+    string r;
+    for ( const char c : s )
+        if ( c != r.back() ) r += c;
+    return r;
+} // T: 0.02+
+
+#elif defined( V2 ) // SC: O(1)
+string removeConsecutiveCharacter( string s ) {
+    size_t i{};
+    for ( size_t j{ 1 }; j < s.size(); ++j )
+        if ( s[ i ] != s[ j ] )
+            s[ ++i ] = s[ j ];
+    return s.substr( 0, ++i );
+} // T: 0.02+
+
+#endif
