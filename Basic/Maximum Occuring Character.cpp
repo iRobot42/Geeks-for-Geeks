25e17709b59e94d45c6e4a99efdd9b98778d50f6
Alexander Kobez <alexander.kobez@gmail.com>
2023-07-22 14:52:20 +0300

    Update Maximum Occuring Character.cpp

--- Basic/Maximum Occuring Character.cpp
+++ Basic/Maximum Occuring Character.cpp
@@ -1,5 +1,5 @@
 char getMaxOccuringChar( string s ) {
-    vector<int> h( 26 );
-    for ( const char c : s ) ++h[ c - 'a' ];
-    return max_element( h.cbegin(), h.cend() ) - h.cbegin() + 'a';
+    int h[ 26 ]{};
+    for ( const char c : s ) ++h[ c-'a' ];
+    return max_element( h, h+26 ) - h + 'a';
 } // T: 0.01+
