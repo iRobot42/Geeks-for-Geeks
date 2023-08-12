22c87c179da8e7596b0ee4c47eb8f42427670b58
Alexander Kobez <alexander.kobez@gmail.com>
2023-08-12 23:03:22 +0300

    Update Count the characters.cpp

--- Basic/Count the characters.cpp
+++ Basic/Count the characters.cpp
@@ -1,6 +1,6 @@
 int getCount( string s, int n ) {
-    int c{}, h[ 26 ]{};
+    int h[ 26 ]{};
     for ( int i{}; i < s.size(); ++i )
-        if ( s[ i ] != s[ i + 1 ] ) ++h[ s[ i ] - 97 ];
-    return count_if( h, h + 26, [ n ]( int i ){ return i == n; } );
+        if ( s[ i ] != s[ i + 1 ]) ++h[ s[ i ] - 97 ];
+    return count( h, h + 26, n );
 } // T: 0.05+
