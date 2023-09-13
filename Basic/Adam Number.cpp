036c472384123b29c387b2fa329447df299dc312
Alexander Kobez <alexander.kobez@gmail.com>
2023-09-13 20:46:08 +0300

    Create Adam Number.cpp

---
+++ Basic/Adam Number.cpp
@@ -0,0 +1,9 @@
+int rev( int n ) {
+    int r{ n % 10 };
+    while ( n /= 10 ) r = r * 10 + n % 10;
+    return r;
+}
+string checkAdamOrNot( int n ) {
+    const int r{ rev( n ) };
+    return n * n == rev( r * r ) ? "YES" : "NO";
+} // 0.02+
