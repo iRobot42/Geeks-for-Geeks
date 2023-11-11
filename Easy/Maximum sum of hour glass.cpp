c51ca6774d351617a5b7af38089a668494b652e2
Alexander Kobez <alexander.kobez@gmail.com>
2023-11-12 00:16:05 +0300

    Create Maximum sum of hour glass.cpp

---
+++ Easy/Maximum sum of hour glass.cpp
@@ -0,0 +1,11 @@
+int findMaxSum( int n, int m, vector< vector< int >> M ) {
+    if ( n-- < 3 || m-- < 3 ) return -1;
+    int s{}, c{ m };
+    while ( --n ) {
+        while ( --m ) s = max( s, M[ n ][ m ] +
+            M[ n-1 ][ m-1 ] + M[ n-1 ][ m ] + M[ n-1 ][ m+1 ] +
+            M[ n+1 ][ m-1 ] + M[ n+1 ][ m ] + M[ n+1 ][ m+1 ] );
+        m = c;
+    }
+    return s;
+}
