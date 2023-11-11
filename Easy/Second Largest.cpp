4f59c29d1d31935c3d304e7fc6f24b581417f068
Alexander Kobez <alexander.kobez@gmail.com>
2023-11-11 03:30:15 +0300

    Create Second Largest.cpp

---
+++ Easy/Second Largest.cpp
@@ -0,0 +1,7 @@
+int print2largest( int a[], int n ) {
+    int l1{ *a }, l2{ -1 };
+    while ( --n )
+        if ( a[ n ] > l2 && a[ n ] != l1 )
+            if (( l2 = a[ n ] ) > l1 ) swap( l1, l2 );
+    return l2;
+}
