33b3cf9e6d0bab51c831d0a1eb160f2612039678
Alexander Kobez <alexander.kobez@gmail.com>
2023-11-12 00:03:03 +0300

    Create Sum of upper and lower triangles.cpp

---
+++ Easy/Sum of upper and lower triangles.cpp
@@ -0,0 +1,8 @@
+vector< int > sumTriangles( const vector< vector< int >>& m, int n ) {
+    int upper{}, lower{};
+    for ( int r{}; r < n; ++r )
+        for ( int c{}; c < n; ++c )
+            if ( r == c ) upper += m[ r ][ c ], lower += m[ r ][ c ];
+            else ( r < c ? upper : lower ) += m[ r ][ c ];
+    return { upper, lower };
+}
