5501ed600f1dd5df4cac8dc3cc35194a87acb2d7
Alexander Kobez <alexander.kobez@gmail.com>
2023-11-14 05:50:40 +0300

    Create Sieve of Eratosthenes.cpp

---
+++ Easy/Sieve of Eratosthenes.cpp
@@ -0,0 +1,12 @@
+vector< int > sieveOfEratosthenes( int n ) {
+    bitset< 10000 > sieve;
+    sieve.set();
+    for ( int i{ 2 }, r{ sqrt( n )}; i <= r; ++i )
+        if ( sieve.test( i ))
+            for ( int m{ i * i }; m <= n; m += i )
+                sieve.reset( m );
+    vector< int > primes;
+    for ( int i{ 2 }; i <= n; ++i )
+        if ( sieve.test( i )) primes.push_back( i );
+    return primes;
+}
