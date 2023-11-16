9c4a100ddc56111d04ae018c962ecc00a83cd934
Alexander Kobez <alexander.kobez@gmail.com>
2023-11-16 20:39:32 +0300

    Update Index Of an Extra Element.cpp

--- Easy/Index Of an Extra Element.cpp
+++ Easy/Index Of an Extra Element.cpp
@@ -1,6 +1,6 @@
 int findExtra( int a[], int b[], int n ) {
-    int l{}, m;
-    while ( l < n ) m = l + n >> 1,
-        a[ m ] == b[ m ] ? l = ++m : n = m;
+    int l{}, r{ --n };
+    while ( l < r ) n = l + r >> 1,
+        a[ n ] == b[ n ] ? l = ++n : r = n;
     return l;
 }
