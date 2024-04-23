ad7274036d7c7e83a3d21247af0d12d1c8e9a09d
Alexander Kobez <alexander.kobez@gmail.com>
2024-04-24 02:29:43 +0300

    Create The Size of.cpp

---
+++ Basic/The Size of.cpp
@@ -0,0 +1,5 @@
+void dataTypes( int a, float b, double c, long long l, string d ) {
+    cout << sizeof( float ) << ' ' << sizeof( double ) << ' '
+         << sizeof( int ) << ' ' << sizeof( long long ) << '\n'
+         << sizeof( d ) << ' ' << sizeof( char ) << endl;
+}
